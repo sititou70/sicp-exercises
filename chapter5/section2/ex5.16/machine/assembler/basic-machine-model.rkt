223b701f8cae4ed93c8908ffd147485074e3475c
Takuya Shizukuishi <sititou70@gmail.com>
2023-06-29 22:01:21 +0900

    feat: ex5.16

---
+++ chapter5/section2/ex5.16/machine/assembler/basic-machine-model.rkt
@@ -0,0 +1,143 @@
+#lang racket
+(provide (all-defined-out))
+
+(require sicp)
+(require "data-structures/register.rkt")
+(require "data-structures/stack.rkt")
+(require "data-structures/instruction.rkt")
+
+; basic machine model
+(define 
+  (make-new-machine)
+  (let 
+    ( ;
+     (pc (make-register 'pc)) ; プログラムカウンタ：現在実行している命令の参照
+     (flag (make-register 'flag))
+     (stack (make-stack))
+     (the-instruction-sequence '())
+     (execution-counter 0)
+     (execution-tracing false)
+    )
+
+    (let 
+      ( ;
+       (the-ops 
+         (list 
+           (list 
+             'initialize-stack
+             (lambda () (stack 'initialize))
+           )
+           (list 
+             'print-and-reset-exec-counter
+             (lambda () 
+               (displayln execution-counter)
+               (set! execution-counter 0)
+             )
+           )
+           (list 
+             'trace-on
+             (lambda () 
+               (set! execution-tracing true)
+             )
+           )
+           (list 
+             'trace-off
+             (lambda () 
+               (set! execution-tracing false)
+             )
+           )
+         )
+       )
+       (register-table 
+         (list 
+           (list 'pc pc)
+           (list 'flag flag)
+         )
+       )
+      )
+      (define 
+        (allocate-register name)
+        (if (assoc name register-table) 
+          (error "Multiply defined register:" name)
+          (set! 
+            register-table
+            (cons (list name (make-register name)) 
+                  register-table
+            )
+          )
+        )
+        'register-allocated
+      )
+      (define 
+        (lookup-register name)
+        (let 
+          ( ;
+           (val (assoc name register-table))
+          )
+
+          (if val 
+            (cadr val)
+            (error "Unknown register:" name)
+          )
+        )
+      )
+      (define 
+        (execute)
+        (set! execution-counter (+ execution-counter 1))
+        (let 
+          ( ;
+           (insts (get-contents pc))
+          )
+
+          (if (null? insts) 
+            'done
+            (begin 
+              (if execution-tracing (displayln (caar insts)))
+              ((instruction-execution-proc (car insts)))
+              (execute)
+            )
+          )
+        )
+      )
+      (define 
+        (dispatch message)
+        (cond 
+          ((eq? message 'start)
+           (set-contents! pc the-instruction-sequence)
+           (execute)
+          )
+          ((eq? message 'install-instruction-sequence)
+           (lambda (seq) 
+             (set! the-instruction-sequence seq)
+           )
+          )
+          ((eq? message 'allocate-register)
+           allocate-register
+          )
+          ((eq? message 'get-register)
+           lookup-register
+          )
+          ((eq? message 'install-operations)
+           (lambda (ops) 
+             (set! the-ops (append the-ops ops))
+           )
+          )
+          ((eq? message 'stack) stack)
+          ((eq? message 'operations) the-ops)
+          (else
+           (error 
+             "Unknown request: MACHINE"
+             message
+           )
+          )
+        )
+      )
+      dispatch
+    )
+  )
+)
+
+(define 
+  (get-register machine reg-name)
+  ((machine 'get-register) reg-name)
+)
